0490cf49cc069ea21176885eeaeab70835e9193d
Mayur <mayur.shetye@netcracker.com>
2020-07-27 21:29:43 +0530

    Added README.MD and enabled Swagger documentation

--- src/main/java/com/questionpro/newsapp/NewsappApplication.java
+++ src/main/java/com/questionpro/newsapp/NewsappApplication.java
@@ -7,10 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAsync
 @EnableCaching
 @EnableScheduling
+@EnableSwagger2
 @SpringBootApplication
 public class NewsappApplication {
 
@@ -21,5 +27,14 @@ public class NewsappApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.questionpro.newsapp"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
